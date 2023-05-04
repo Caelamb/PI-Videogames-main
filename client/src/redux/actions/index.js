74bf07f8289a2bd06efc560d7a705007ce33aad7
Caelmab <barrylamb530@gmail.com>
2023-05-03 19:13:48 -0600

    08 commit

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -7,22 +7,12 @@ export const FILTER_GAMES_BY_ORIGIN = 'FILTER_GAMES_BY_ORIGIN';
 export const SORT_GAMES = 'SORT_GAMES';
 export const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
 
-export const GET_USERS = "GET_USERS";
-
-export const getUsers = () => {
-    return async (dispatch) => {
-        const response = (await axios.get("https://jsonplaceholder.typicode.com/users")).data;
-        return dispatch({
-            type: "GET_USERS",
-            payload: response
-        })
-    }
-};
+const BASE_URL = 'http://localhost:3001';
 
 // Action para buscar videojuegos por nombre
 export const searchGames = (searchTerm) => async (dispatch) => {
     try {
-      const res = await axios.get(`/videogames?name=${searchTerm}`);
+      const res = await axios.get(`${BASE_URL}/videogames?name=${searchTerm}`);
       dispatch({ type: SEARCH_GAMES, payload: res.data });
       return res.data;
     } catch (err) {
@@ -34,7 +24,7 @@ export const searchGames = (searchTerm) => async (dispatch) => {
   // Action para obtener los primeros 100 videojuegos desde la ruta GET /videogames
   export const fetchGames = (page) => async (dispatch) => {
     try {
-      const res = await axios.get(`/videogames?page=${page}`);
+      const res = await axios.get(`${BASE_URL}/videogames?page=${page}`);
       dispatch({ type: FETCH_GAMES, payload: res.data });
       return res.data;
     } catch (err) {
@@ -46,7 +36,7 @@ export const searchGames = (searchTerm) => async (dispatch) => {
   // Action para filtrar los videojuegos por género
   export const filterGamesByGenre = (genre) => async (dispatch) => {
     try {
-      const res = await axios.get(`/videogames?genre=${genre}`);
+      const res = await axios.get(`${BASE_URL}/genres?genre=${genre}/videogames`);
       dispatch({ type: FILTER_GAMES_BY_GENRE, payload: res.data });
       return res.data;
     } catch (err) {
@@ -58,7 +48,7 @@ export const searchGames = (searchTerm) => async (dispatch) => {
   // Action para filtrar los videojuegos por origen (API o base de datos)
   export const filterGamesByOrigin = (origin) => async (dispatch) => {
     try {
-      const res = await axios.get(`/videogames?origin=${origin}`);
+      const res = await axios.get(`${BASE_URL}/videogames?origin=${origin}`);
       dispatch({ type: FILTER_GAMES_BY_ORIGIN, payload: res.data });
       return res.data;
     } catch (err) {
@@ -70,7 +60,7 @@ export const searchGames = (searchTerm) => async (dispatch) => {
   // Action para ordenar los videojuegos por nombre o por rating, ascendente o descendente
   export const sortGames = (sortBy, sortOrder) => async (dispatch) => {
     try {
-      const res = await axios.get(`/videogames?sortBy=${sortBy}&sortOrder=${sortOrder}`);
+      const res = await axios.get(`${BASE_URL}/videogames?sortBy=${sortBy}&sortOrder=${sortOrder}`);
       dispatch({ type: SORT_GAMES, payload: res.data });
       return res.data;
     } catch (err) {
